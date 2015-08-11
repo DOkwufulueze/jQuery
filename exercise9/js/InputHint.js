df8d376e81da368bfc9f92c215cb257f23063a9b
Daniel Okwufulueze <daniel@dealdey.com>
2015-08-11 13:03:57 +0100

    Implement Loading of external content

---
+++ exercise9/js/InputHint.js
@@ -0,0 +1,76 @@
+'use strict'
+
+class InputHint {
+
+  //InputHint Constructor
+  constructor() {
+    this._$label = $('label[for="q"]');
+    this._$input = $('input[name="q"]');
+    this._init();
+  }
+
+  //Entry Point
+  _init() {
+    this._implementInputHint(this._$input, this._$label);
+  }
+
+  _implementInputHint(input, label) {
+    this._searchInputValueToLabelText(input, label);
+  }
+
+  _searchInputValueToLabelText(input, label) {
+    input
+      .val(label.text())
+      .addClass('hint')
+      .bind({
+        'focus': () => {this._removeHintText(input, label);},
+        'blur': () => {this._restoreHintText(input, label);},
+      })
+      .prev('label').remove();
+  }
+
+  _removeHintText(input, label) {
+    if (this._isInputHintString(input, label)) {
+      this._performRemovalofHint(input, label);
+    }
+  }
+
+  _restoreHintText(input, label) {
+    if (this._isInputEmptyString(input)) {
+      this._performRestoreOfHint(input, label);
+    }
+  }
+
+  _performRemovalofHint(input, label) {
+    input
+      .val('')
+      .removeClass('hint');
+  }
+
+  _performRestoreOfHint(input, label) {
+    input
+      .val(label.text())
+      .addClass('hint');
+  }
+
+  _isInputHintString(input, label) {
+    if (input.val().trim() === label.text()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  _isInputEmptyString(input) {
+    if (input.val().trim() === '') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+$(() => {
+  new InputHint();
+});
+
