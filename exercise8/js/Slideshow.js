2c7ecccedbb5c5662f43c11886269f6794bbdebd
Daniel Okwufulueze <daniel@dealdey.com>
2015-08-12 16:51:25 +0100

    Implement Slideshow

--- exercise8/js/Slideshow.js
+++ exercise8/js/Slideshow.js
@@ -3,110 +3,86 @@
 class Slideshow {
 
   //Slideshow constructor
-  constructor() {
-    this._$slideshow = $('ul#slideshow');
-    this._$slideshowDiv = $('<div id="slideshowDiv"> </div>');
-    this._$allButTheFirstListItem = $('ul#slideshow li:gt(0)');
-    this._$navigation = $('<div class="navigation"> </div>');
+  constructor($slideshowArea, $navigation) {
+    this._$slideItems = $slideshowArea.find('li');
+    this._$firstSlideItem = this._$slideItems.first();
+    this._$allButTheFirstListItem = this._$slideItems.slice(1);
+    this._slideShowSize = this._$slideItems.length;
+    this._$slideshowArea = $slideshowArea;
+    this._$navigation = $navigation;
     this._init();
   }
 
   _init() {
-    this._moveSlideshowToTop(this._$slideshowDiv);
+    this._moveSlideshowToTop();
+    this._addNavigationArea();
+    this._beginSlideshow();
   }
 
-  _moveSlideshowToTop(slideshowDiv) {
-    slideshowDiv
-      .css({
-        height: '400px',
-        overflow: 'hidden',
-        width: '100%',
-      })
-      .append(this._$slideshow)
-      .prependTo('body');
-    this._beginSlideshow(this._$slideshow.children('li'));
-    this._addNavigationArea(this._$navigation, this._$slideshow);
+  _moveSlideshowToTop() {
+    this._$slideshowArea.prependTo('body');
   }
 
-  _addNavigationArea(navigation, slideshow) {
-    navigation
+  _addNavigationArea() {
+    this._$navigation
     .css({
       background: '#bababa',
       width: '100%',
       marginBottom: '30px',
     })
-    .insertAfter(slideshow.parent('div'));
-    this._showSizeInNavigation(navigation, this._getSize(slideshow.children('li')));
-    this._showImageViewed(this._$navigation, this._$slideshow.children('li').first());
+    .insertAfter(this._$slideshowArea);
   }
 
-  _showSizeInNavigation(navigation, size) {
-    navigation
-    .html(`Total Number of Images: <b>${size}</b>.    Image Viewed: <span style='font-weight:bold;' class='image'> </span>`);
+  _showSlideShowSizeInNavigation() {
+    this._$navigation
+    .html(`${nextItem+1} of ${this._slideShowSize}... Image: ${this._showImageViewed()}`);
   }
 
-  _beginSlideshow(listItems) {
-    this._hideOtherListItems(this._$allButTheFirstListItem);
-    setInterval(() => {
-      this._showNext(listItems);
-    }, 3000);
+  _beginSlideshow() {
+    this._$allButTheFirstListItem.css('display', 'none');
+    this._showSlideShowSizeInNavigation();
+    this._$firstSlideItem
+      .delay(3500)
+      .fadeOut(500, () => {
+        this._showNextSlideItem();
+      });
   }
 
-  _hideOtherListItems(allButTheFirstListItem) {
-    allButTheFirstListItem.css('display', 'none');
+  _showNextSlideItem() {
+    nextItem += 1;
+    nextItem = this._resetNextItemIfOverflow();
+    this._showSlideShowSizeInNavigation();
+    this._revealSlideItem();
   }
 
-  _showNext(listItems) {
-    itemIndex = this._reset(itemIndex, this._getSize(listItems));
-    const $currentItem = itemIndex > 0 ? $(listItems.get(itemIndex-1)) : $(listItems.get(itemIndex));
-    const $nextItem = $(listItems.get(itemIndex));
-    this._testItemAndShow($currentItem, $nextItem);
-    itemIndex++;
+  _revealSlideItem() {
+    this._$slideItems.eq(nextItem)
+      .fadeIn(500)
+      .delay(3000)
+      .fadeOut(500, () => {
+        this._showNextSlideItem();
+      });
   }
 
-  _reset(itemIndex, size) {
-    if (itemIndex === size ) {
-      this._hideItem($('ul#slideshow li').last(), 5);
-      itemIndex = 0;
+  _resetNextItemIfOverflow() {
+    if (nextItem === this._slideShowSize) {
+      nextItem = 0;
     }
-    return itemIndex;
+    return nextItem;
   }
 
-  _getSize(listItems) {
-   return listItems.length;
-  }
-
-  _hideItem(listItem, duration) {
-   listItem.fadeOut(duration);
-  }
-
-  _showItem(listItem, duration) {
-   listItem.fadeIn(duration);
-  }
-
-  _testItemAndShow (currentItem, nextItem) {
-    if (currentItem !== nextItem) {
-      this._hideItem(currentItem, 2);
-      this._showItem(nextItem, 1500);
-    } else {
-      this._showItem(nextItem, 1500);
-    }
-
-    this._showImageViewed(this._$navigation, nextItem);
-  }
-
-  _showImageViewed(navigation, item) {
-    navigation
-    .children('span.image')
-    .html(item
-      .children('img')
-      .attr('alt')
-    );
+  _showImageViewed() {
+    return this._$slideItems.eq(nextItem)
+      .find('img')
+      .attr('alt');
   }
 }
 
-let itemIndex = 1;
+let nextItem = 0;
 $(() => {
-  new Slideshow();
+  const $slideshowArea = $('ul#slideshow');
+  $slideshowArea.css({ 'height': '400px', });
+  const $navigation = $('<div />', {'class': 'navigation',});
+  new Slideshow($slideshowArea, $navigation);
 });
 
