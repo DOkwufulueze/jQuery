b772495853ed4a12234b57e9fb778027f4d6bea6
Daniel Okwufulueze <daniel@dealdey.com>
2015-08-14 11:31:59 +0100

    Remove extra files. Correct element creation construction. Handle all css in style.css

--- exercise12/js/DivStack.js
+++ exercise12/js/DivStack.js
@@ -12,11 +12,11 @@ class DivStack {
 
   //Entry Point
   _init() {
-    this._beginAppending();
+    this._beginInitialAppending();
     this._addEventListener();
   }
 
-  _beginAppending() {
+  _beginInitialAppending() {
     this._$emptyContainer.append(this._$button);
     this._$body.append(this._$emptyContainer);
   }
@@ -24,34 +24,26 @@ class DivStack {
   _addEventListener() {
     this._$emptyContainer.on('click', (eventObjet) => {
       const $target = $(eventObjet.target);
-      this._useTarget($target);
+      this._checkTargetTagType($target);
     });
-  }
-
-  _useTarget(target) {
-    this._checkTargetTagType(target);
   }
 
   _checkTargetTagType(target) {
     if (target.not('.emptyContainer').is('div')) {
-      this._ManipulateDiv(target);
+      this._manipulateDiv(target);
     } else if (target.is('input')) {
       this._$emptyContainer.append($('<div />', {'class': 'newDiv','html': `DIV NUMBER ${counter}`}));
       counter += 1;
     }
   }
 
-  _ManipulateDiv(target) {
-    target.is(this._$emptyContainer.children('div').last()) ? this._removeDiv(target) : this._highlightDiv(target);
+  _manipulateDiv(target) {
+    target.is(this._$emptyContainer.children('div').last()) ? this._removeDiv(target) : target.addClass('highlightedDiv');
   }
 
   _removeDiv(target) {
     target.remove();
     counter -= 1;
-  }
-
-  _highlightDiv(target) {
-    target.addClass('highlightedDiv');
   }
 }
 
