c2fc0f90668a22c9904b2519c8b5e709e13da1cb
Daniel Okwufulueze <daniel@dealdey.com>
2015-08-18 12:22:49 +0100

    Use shorter method name

--- exercise6/js/DisplayManager.js
+++ exercise6/js/DisplayManager.js
@@ -16,24 +16,14 @@ class DisplayManager {
     this._$blog.on('click', (eventObject) => {
       eventObject.preventDefault();
       const $target = $(eventObject.target);
-      if (this._isHeadlineClicked($target)) {
-        this._toggleExcerpt($target);
-      }
+      $target.is('a') ? this._toggleExcerpt($target) : null;
     });
-  }
-
-  _isHeadlineClicked(target) {
-    if (target.is('a')) {
-      return true;
-    } else {
-      return false;
-    }
   }
 
   _toggleExcerpt(target) {
     const $targetExcerpt = target.parents('h3').siblings('p.excerpt');
     this._slideUpExcerpts(target);
-    this._slideDownExcerpt($targetExcerpt);
+    $targetExcerpt.slideDown(1000);
   }
 
   _slideUpExcerpts(target) {
@@ -42,10 +32,6 @@ class DisplayManager {
       .siblings('li')
       .children('p.excerpt')
       .slideUp(1000);
-  }
-
-  _slideDownExcerpt(targetExcerpt) {
-    targetExcerpt.slideDown(1000);
   }
 }
 
